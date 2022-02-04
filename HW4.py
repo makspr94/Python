622c75c4b58abf99eb53eeb4a4e41b3246dd5497
m.prybylski <makspr94@gmail.com>
2022-02-04 17:01:06 +0300

    test commit

--- HW4.py
+++ HW4.py
@@ -1,7 +1,8 @@
 currency_amount = int(input('enter your amount:'))
 byn_item = 'byn'
 usd_item = 'usd'
-usd_byn_rate = 2.58
+usd_byn_rate = 2.5
+usd_byn
 exchange_result = currency_amount / usd_byn_rate
 print('you have entered', currency_amount, byn_item)
-print( 'converted amount in USD =', exchange_result )
+print( 'converted amount in USD =', exchange_result)
--- Stepik.py
+++ Stepik.py
@@ -7,3 +7,34 @@
 #     list.remove(min(list))
 # print(list)
 
+# s = input()
+# words = s.split()
+# print(words[0][0], words[1][0], words[2][0], sep = '.')
+#
+
+
+#s = input().split("\\")
+#print(*s, sep = '\n')
+
+# numbers = [8, 9, 10, 11]
+# del numbers[1]
+# numbers.insert(1, 17)
+# numbers.extend([4, 5, 6])
+# del numbers[0]
+# numbers.extend(numbers)
+# numbers.insert(3,25)
+# print(numbers)
+
+s = input().split()
+s = [int(x) for x in s]
+max_index = s.index(max(s))
+min_index = s.index(min(s))
+s[max_index], s[min_index] = s[min_index], s[max_index]
+# s = [str(x) for x in s]
+# s = ' '.join(s)
+print(*s)
+
+
+
+
+
